367bdd6b8bf9a48244468fa7c65c0baa0b668a5d
abduvalimurodullayev <abduvalimurodullayev9@gmail.com>
2025-01-08 14:17:58 +0500

    Describe your changes, e.g., Updated admin.py and jazzmin settings

--- apps/reservation/admin.py
+++ apps/reservation/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.db.models import JSONField
+from .models import Restaurant, Reservation, User
+from prettyjson.widgets import PrettyJSONWidget
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('title', 'address', 'average_rating', 'available_sites')  
+    list_filter = ('average_rating', 'available_sites')  
+    search_fields = ('title', 'address', 'description')  
+    ordering = ('-average_rating',)  
+    readonly_fields = ('average_rating', 'reviews')  
+    formfield_overrides = {
+        JSONField: {'widget': PrettyJSONWidget}  # JSON maydonlar uchun chiroyli editor
+    }
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'restaurant', 'reservation_time', 'number_people')  
+    list_filter = ('reservation_time', 'restaurant')  
+    search_fields = ('user__phone_number', 'restaurant__title')  
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'is_active', 'is_superuser', 'is_staff')  
+    list_filter = ('is_active', 'is_staff')  
+    search_fields = ('phone_number',)  
+    ordering = ('phone_number',)  
