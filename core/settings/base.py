0615134848712495b0490d2ecad17811c14f313e
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2025-01-08 13:22:56 +0500

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -3,17 +3,11 @@ from pathlib import Path
 
 import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-# READING ENV
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, ".env"))
 from .jazzmin import *
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
