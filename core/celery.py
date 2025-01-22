af69fbcc3ebf762837ba614f02b4eb3d29e3c4b3
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2025-01-22 13:37:51 +0500

    Update celery.py

--- core/celery.py
+++ core/celery.py
@@ -9,7 +9,7 @@ app = Celery("core")
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes
-# - namespace='CELERY' 
+# - namespace='CELER
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
