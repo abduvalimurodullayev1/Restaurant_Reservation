e7af624637f84f5afe4242f23b9e6795dcec18c8
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2025-01-22 13:38:37 +0500

    Update celery.py

--- core/celery.py
+++ core/celery.py
@@ -7,7 +7,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.base")
 
 app = Celery("core")
 
-# Using a string here means the worker doesn't have to serialize
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django apps.
