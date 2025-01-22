a84aa3d7b4b7720c2e2127e3bcac7602a38ee59e
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2025-01-22 13:38:09 +0500

    Update celery.py

--- core/celery.py
+++ core/celery.py
@@ -8,7 +8,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.base")
 app = Celery("core")
 
 # Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes
+# the configuration 
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
